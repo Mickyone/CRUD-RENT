2ac2e4500bd6827a80aa03b9a7aff5cd8dc730a9
Micky <mickywan@hotmail.fr>
2023-11-15 09:13:13 +0100

    add function 'upload image' functions.php

--- config/functions.php
+++ config/functions.php
@@ -14,3 +14,41 @@ function dd($value)
     dbug($value);
     die('Script php arrété !!!');
 }
+
+function uploadImage($image) {
+    // Validate the image
+    if (!$image['error']) {
+        // Get image extension
+        $imageExtension = strtolower(pathinfo($image['name'], PATHINFO_EXTENSION));
+
+        // Allow only certain file types
+        $allowedExtensions = ['jpg', 'jpeg', 'png', 'gif'];
+        if (!in_array($imageExtension, $allowedExtensions)) {
+            throw new Exception('Invalid image format');
+        }
+
+        // Generate a unique filename
+        $imageFilename = uniqid() . '.' . $imageExtension;
+
+        // Upload the image
+        if (!move_uploaded_file($image['tmp_name'], '../uploads/' . $imageFilename)) {
+            throw new Exception('Failed to upload image');
+        }
+
+        // Return the uploaded image filename
+        return $imageFilename;
+    } else {
+        // Handle image upload error
+        if ($image['error'] === UPLOAD_ERR_INI_SIZE) {
+            throw new Exception('The image is too large');
+        } elseif ($image['error'] === UPLOAD_ERR_FORM_SIZE) {
+            throw new Exception('The image is too large (exceeds form limit)');
+        } elseif ($image['error'] === UPLOAD_ERR_PARTIAL) {
+            throw new Exception('The image was only partially uploaded');
+        } elseif ($image['error'] === UPLOAD_ERR_NO_FILE) {
+            throw new Exception('No image was selected');
+        } else {
+            throw new Exception('An unknown error occurred while uploading the image');
+        }
+    }
+}
