023cdd0a47b597b30b7e4d580ec463ccf7db564e
Micky <mickywan@hotmail.fr>
2023-11-15 09:17:01 +0100

    add submit button new-rent

--- controllers/new-rent.php
+++ controllers/new-rent.php
@@ -7,13 +7,13 @@ $users = $connexion->query($requete)->fetchAll();
 if ($_SERVER['REQUEST_METHOD'] === 'POST'){
     $errors = [];
 
-    $id = trim(filter_var($_POST['id'], FILTER_SANITIZE_FULL_SPECIAL_CHARS));
+    // $id = trim(filter_var($_POST['id'], FILTER_SANITIZE_FULL_SPECIAL_CHARS));
     $registration = trim(filter_var($_POST['registration'], FILTER_SANITIZE_FULL_SPECIAL_CHARS));
     $marque = trim(filter_var($_POST['brand'], FILTER_SANITIZE_FULL_SPECIAL_CHARS));
     $type = trim(filter_var($_POST['type'], FILTER_SANITIZE_FULL_SPECIAL_CHARS));
     $capacity = trim(filter_var($_POST['capacity'], FILTER_SANITIZE_FULL_SPECIAL_CHARS));
-    $rent_at = trim(filter_var($_POST['rent_at'], FILTER_SANITIZE_FULL_SPECIAL_CHARS));
-    $back_at = trim(filter_var($_POST['back_at'], FILTER_SANITIZE_FULL_SPECIAL_CHARS));
+    // $rent_at = trim(filter_var($_POST['rent_at'], FILTER_SANITIZE_FULL_SPECIAL_CHARS));
+    // $back_at = trim(filter_var($_POST['back_at'], FILTER_SANITIZE_FULL_SPECIAL_CHARS));
 }
 
 
--- views/new-rent.view.php
+++ views/new-rent.view.php
@@ -3,8 +3,7 @@
 <h2>Add a new rent</h2>
 
 <form method="POST" enctype="multipart/form-data">
-    <label for="id">ID :</label>
-    <input type="text" name="id" id="id" value="<?= isset($_POST['id']) ? $_POST['id'] : '' ?>">
+
 
     <label for="registration">Registration :</label>
     <input type="text" name="registration" id="registration" value="<?= isset($_POST['registration']) ? $_POST['registration'] : '' ?>">
@@ -18,8 +17,4 @@
     <label for="capacity">Capacity :</label>
     <input type="text" name="capacity" id="capacity" value="<?= isset($_POST['capacity']) ? $_POST['capacity'] : '' ?>">
 
-    <label for="Rent_at">Rent at :</label>
-    <input type="text" name="Rent_at" id="Rent_at" value="<?= isset($_POST['Rent_at']) ? $_POST['Rent_at'] : '' ?>">
-
-    <label for="Back_at">Back_at :</label>
-    <input type="text" name="Back_at" id="Back_at" value="<?= isset($_POST['Back_at']) ? $_POST['Back_at'] : '' ?>">
+    <input type="submit" name="submitted" value="submit">
